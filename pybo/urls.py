688a615bfcaee898880ed2090457a69533ec1582
YEEmagic <sehyun5698@gmail.com>
2024-07-19 10:13:53 +0900

    changed to standard HTML structure

--- pybo/urls.py
+++ pybo/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # ''안의 내용이 pybo/(뒤에 오는 내용) <<이 된다
     path('<int:question_id>/', views.detail, name='detail'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('question/create/', views.question_create, name='question_create'),
 ]
