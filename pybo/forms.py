7596c18e9fc3ce56968963ebfb9e44439b64be29
YEEmagic <sehyun5698@gmail.com>
2024-07-23 23:14:47 +0900

    added attributes of widget

--- pybo/forms.py
+++ pybo/forms.py
@@ -5,3 +5,7 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question # 사용할 모델
         fields = ['subject', 'content'] # Ques-Form에서 사용할 Question 모델의 속성
+        widgets = {
+            'subject' : forms.TextInput(attrs={'class' : 'form-control'}),
+            'content' : forms.Textarea(attrs={'class' : 'form-control', 'rows':10}),
+        }
